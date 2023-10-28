5f5803b56d3b87796f200a5f7ebee77982c98d39
Omar Eiyana <omar.e@opendeusto.es>
2023-10-28 18:05:25 +0200

    /

--- src/domain/Empleado.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public class Empleado extends Persona{
-
-}
--- src/domain/TipoAlimentacion.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public enum TipoAlimentacion {
-	CARNICOS, VEGETALES, BEBIDAS, CONGELADOS, DULCES;
-}
